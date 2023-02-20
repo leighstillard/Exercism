27ea4172566bdb257aaad1870587379541b30a61
Leigh Stillard <leigh@stillard.com>
2023-02-21 10:24:38 +1100

    Started learning via the Elixir track

---
+++ elixir/lasagna/lib/lasagna.ex
@@ -0,0 +1,20 @@
+defmodule Lasagna do
+  def expected_minutes_in_oven do
+    40
+  end
+  def remaining_minutes_in_oven(actual_minutes) do
+    expected_minutes_in_oven() - actual_minutes
+  end
+
+  def preparation_time_in_minutes(layers) do
+    layers * 2
+  end
+
+  def total_time_in_minutes(layers, actual_minutes) do
+    preparation_time_in_minutes(layers) + actual_minutes
+  end
+
+  def alarm do
+    "Ding!"
+  end
+end
